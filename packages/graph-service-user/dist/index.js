ea061f6fc3841c265c1e1315a0cc650a2e6e7471
Amisha Gupta <amishagupta@stockx.com>
2022-10-19 15:56:00 +0530

    message

--- packages/graph-service-user/dist/index.js
+++ packages/graph-service-user/dist/index.js
@@ -2,8 +2,8 @@
 exports.__esModule = true;
 var buildSubgraphSchema_1 = require("@apollo/subgraph/dist/buildSubgraphSchema");
 var apollo_server_1 = require("apollo-server");
-var schema_graphql_1 = require("./src/query/schema.graphql");
-var resolver_1 = require("./src/query/resolver");
+var schema_graphql_1 = require("./query/schema.graphql");
+var resolver_1 = require("./query/resolver");
 var server = new apollo_server_1.ApolloServer({
     schema: (0, buildSubgraphSchema_1.buildFederatedSchema)([{ typeDefs: schema_graphql_1.typeDefs, resolvers: resolver_1.resolvers }])
 });
